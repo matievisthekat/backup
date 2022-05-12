ac767c0a51b5c8b362f1c5283e5d2dc5fb141dbb
matievisthekat <matievisthekat@gmail.com>
2022-05-12 21:00:59 +0200

    remove main sub

--- Backup.pl
+++ Backup.pl
@@ -8,22 +8,17 @@ use Getopt::Long;
 
 Readonly our $VERSION => q{0.0.0};
 
-exit main();
+my $verbose = 0;
+my $help    = 0;
+my $version = 0;
+GetOptions(
+    'verbose' => \$verbose,
+    'help'    => \$help,
+    'version' => \$version,
+) or die();
 
-sub main {
-    my $verbose = 0;
-    my $help    = 0;
-    my $version = 0;
-    GetOptions(
-        'verbose' => \$verbose,
-        'help'    => \$help,
-        'version' => \$version,
-    ) or die("Error in command line arguments\n");
-
-    if ($version) {
-        print "Backup.pl version $VERSION\n";
-        return 0;
-    }
-
-    return 0;
+if ($version) {
+    print "Backup.pl version $VERSION\n";
 }
+
+1;
