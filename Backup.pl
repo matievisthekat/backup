caaf69f83a41746b7ae2021a574de13628f668f6
matievisthekat <matievisthekat@gmail.com>
2022-05-12 20:52:45 +0200

    listen to perlcritic and implement --version flag

---
+++ Backup.pl
@@ -0,0 +1,31 @@
+package Backup;
+use strict;
+use warnings;
+use Readonly;
+
+# use CLI::Helpers qw(:all);
+use Getopt::Long;
+
+Readonly our $VERSION => q{0.0.0};
+
+my $exit_code = main();
+
+sub main {
+    my $verbose = 0;
+    my $help    = 0;
+    my $version = 0;
+    GetOptions(
+        'verbose' => \$verbose,
+        'help'    => \$help,
+        'version' => \$version,
+    ) or die("Error in command line arguments\n");
+
+    if ($version) {
+        print "Backup.pl version $VERSION\n";
+        return 0;
+    }
+
+    return 0;
+}
+
+1;
