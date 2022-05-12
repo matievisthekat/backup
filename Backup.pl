ea3c9444d93ba1a4d85d6660c7de8606b558113f
matievisthekat <matievisthekat@gmail.com>
2022-05-12 21:17:17 +0200

    remove CLI::Helpers and implement verbose() and debug() subs

--- Backup.pl
+++ Backup.pl
@@ -3,21 +3,32 @@ use strict;
 use warnings;
 use Readonly;
 
-# use CLI::Helpers qw(:all);
 use Getopt::Long;
 
+Readonly our $AUTHOR  => "Matthew Stead <matievisthekat\@gmail.com>";
 Readonly our $VERSION => q{0.0.0};
 
-my $verbose = 0;
-my $help    = 0;
-my $version = 0;
-GetOptions(
+our $verbose = 0;
+our $debug   = 0;
+our $help    = 0;
+our $version = 0;
+our $args    = GetOptions(
     'verbose' => \$verbose,
+    'debug'   => \$debug,
     'help'    => \$help,
     'version' => \$version,
 ) or die();
 
+sub verbose {
+    print @_ if $verbose;
+}
+
+sub debug {
+    print @_ if $debug;
+}
+
 if ($version) {
+    debug "\$VERSION=$VERSION\n";
     print "Backup.pl version $VERSION\n";
 }
 
